1280f8fb134776da79d8bb6e0a6c523896d66322
Matthew Lui <matthew.l@binance.com>
2022-05-16 12:33:22 +0800

    added websocket builder

--- Sources/VaporRouteBuilder/WebsocketRoute.swift
+++ Sources/VaporRouteBuilder/WebsocketRoute.swift
@@ -2,4 +2,22 @@
 // Created by Matthew Lui on 16/5/2022.
 //
 
-import Foundation
+import Vapor
+
+struct WebSocket: RouteBuildable {
+    public var route: BuildableRoute
+
+    public init(_ path: PathComponent...,
+                maxFrameSize: WebSocketMaxFrameSize = .`default`,
+                shouldUpgrade: @escaping ((Request) -> EventLoopFuture<HTTPHeaders?>) = {
+                    $0.eventLoop.makeSucceededFuture([:])
+                },
+                onUpgrade: @escaping (Request, WebSocket) -> ()
+    ) {
+        route = { builder in
+            builder.on(.GET, path) { (request: Request) -> Response in
+                request.webSocket(maxFrameSize: maxFrameSize, shouldUpgrade: shouldUpgrade, onUpgrade: onUpgrade)
+            }
+        }
+    }
+}
